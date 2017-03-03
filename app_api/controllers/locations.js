724741affa6a12688476f8611fac437f823ab9d9
Andrew Allshouse <allshouseandrew@aol.com>
2017-03-03 17:28:39 -0500

    Add geolocation to distance API and saving to db on the create API

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -2,30 +2,93 @@
 var mongoose = require('mongoose');
 var loc = mongoose.model('location');
 
+var theEarth = (function(){
+  var earthRadius = 6371; //km
+  var getDistanceFromRads = function(rads){
+    return parseFloat(rads * earthRadius);
+  };
+  var getRadsFromDistance = function(distance){
+    return parseFloat(distance / earthRadius);
+  };
+
+  return {
+    getDistanceFromRads: getDistanceFromRads,
+    getRadsFromDistance: getRadsFromDistance
+  };
+})();
+
 var sendJsonResponse = function(res, status, content){
   res.status(status);
   res.json(content);
 };
 
 module.exports.listByDistance = function(req, res){
-  loc.find()
-    .exec(function(err, locations){
-      if(!locations || locations.length === 0){
-        sendJsonResponse(res, 404, {"message": "no locations found"});
-        return;
-      }
+  var lng = parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
+  var maxDistance = parseFloat(req.query.maxDistance);
 
-      if(err){
-        sendJsonResponse(res, 404, err);
-        return;
-      }
-
-      sendJsonResponse(res, 200, locations);
+  if(!lat || !lng || !maxDistance){
+    sendJsonResponse(res, 404, {
+      message: 'lng, lat, and maxDistance query parameters are required.'
     });
+    return;
+  }
+
+  var point = {
+    type: 'Point',
+    coordinates: [lng, lat]
+  };
+  var geoOptions = {
+    spherical: true,
+    maxDistance: theEarth.getRadsFromDistance(maxDistance),
+    num: 10
+  };
+
+  loc.geoNear(point, geoOptions, function(err, results, stats){
+    if(err){
+      sendJsonResponse(res, 404, err);
+      return;
+    }
+    
+    var locations = [];
+    results.forEach(function(doc){
+      locations.push({
+        distance: theEarth.getDistanceFromRads(doc.dis),
+        name: doc.obj.name,
+        address: doc.obj.address,
+        rating: doc.obj.rating,
+        facilities: doc.obj.facilities,
+        _id: doc.obj._id
+      });
+    });
+    sendJsonResponse(res, 200, locations);
+  });
 };
 
 module.exports.create = function(req, res){
-  sendJsonResponse(res, 200, {"status": "success"});
+  loc.create({
+    name: req.body.name,
+    address: req.body.address,
+    facilities: req.body.facilities.split(","),
+    coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+    openingTimes: [{
+      days: req.body.days1,
+      opening: req.body.opening1,
+      closing: req.body.closing1,
+      closed: req.body.closed1
+    }, {
+      days: req.body.days2,
+      opening: req.body.opening2,
+      closing: req.body.closing2,
+      closed: req.body.closed2
+    }]
+  }, function(err, location){
+    if(err){
+      sendJsonResponse(res, 400, location);
+    } else {
+      sendJsonResponse(res, 201, location);
+    }
+  });
 };
 
 module.exports.read = function(req, res){
